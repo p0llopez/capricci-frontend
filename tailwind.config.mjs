c86bcc1b43a9983508e990a59335e1b13f10ff16
Pol Lopez <pol.lopez.cano@gmail.com>
2024-05-01 23:59:10 +0200

    feat: Header done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    theme: {
-        extend: {
-            colors: {
-                bluegray: "#232F3F",
-                bluegreen: {
-                    70: "#71A9A9",
-                    DEFAULT: "#378888",
-                },
-            },
-            screens: {
-                xs: "380px",
-            },
-        },
-    },
-    plugins: [],
-};
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				bluegray: "#232F3F",
+				bluegreen: {
+					70: "#71A9A9",
+					DEFAULT: "#378888",
+				},
+				beige: "#FCFCFC",
+			},
+			screens: {
+				xs: "380px",
+			},
+		},
+	},
+	plugins: [],
+}
