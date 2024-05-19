2aa01d6cb054e480ebd1ec2a2fafbde3c038d914
Pol Lopez <pol.lopez.cano@gmail.com>
2024-05-19 22:46:35 +0200

    feat: deploy on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
-import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
-import { defineConfig } from "astro/config"
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
-	image: {
-		remotePatterns: [
-			{
-				protocol: "https",
-			},
-		],
-		domains: ["cdn.capriccipineda.es"],
-	},
-	output: "server",
-	adapter: node({
-		mode: 'standalone',
-	}),
-})
+  integrations: [tailwind(), react()],
+  image: {
+    remotePatterns: [{
+      protocol: "https"
+    }],
+    domains: ["cdn.capriccipineda.es"]
+  },
+  output: "server",
+  adapter: vercel()
+});
